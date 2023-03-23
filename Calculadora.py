152980d8099e5460ab1ee1544795d9b7871acc86
Kauerico <ribeirokaue70@gmail.com>
2023-03-23 16:46:20 -0300

    Base calculadora.

--- Calculadora.py
+++ Calculadora.py
@@ -2,11 +2,36 @@ print ('x' * 12)
 print ('Calculadora')
 print ('x' * 12)
 
+# Entrada de numeros int ou float.
 
-# Entradas do usuário
+numero_1 = int and float(input('Informe o primeiro número: ').strip().replace(',' , '.'))
+numero_2 = int and float(input('Informe o segundo número: ').strip().replace(',' , '.'))
 
-quantidade_numeros = (input('Quantos numeros serão calculados? :  ')).strip()
+# Escolha da operação Matematica.
 
-for contador in range (quantidade_numeros):
-    contador += 1
-    num
+escolha_operacao = input ('''Escolha uma das opações abaixo:\n
+( + ) = Soma \n( - ) = Subtração \n( / ) = divisão \n( x ) = Multiplicação \n
+Sua escolha? : ''').strip()
+
+# Realizaçao do calculo com math case.
+
+match escolha_operacao:
+    case ('+'):
+        resultado = numero_1 + numero_2
+    case ('-'):
+        resultado = numero_1 - numero_2
+    case ('/'):
+        resultado = numero_1 / numero_2
+    case ('x'):
+        resultado = numero_1 * numero_2 
+
+print (f'Resultado:\n{numero_1} {escolha_operacao} {numero_2} = {resultado}')
+
+
+# while True:
+#     quantidade_num = int(input('Quantos números serão calculados? : ').strip())
+    
+# # O for vai se repetir quantas o usuario quiser.
+#     for numero in range(quantidade_num):
+#         numero = input(f'Informe o {numero + 1}° número: ').strip().replace(',', '.')
+        
