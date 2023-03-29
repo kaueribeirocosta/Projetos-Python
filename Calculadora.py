884046690f2f45e73cc2f8a38d3fbbacad61b9f0
Kauerico <ribeirokaue70@gmail.com>
2023-03-29 08:53:59 -0300

    versao 3

--- Calculadora.py
+++ Calculadora.py
@@ -4,10 +4,16 @@ print ('x' * 12)
 
 from time import sleep
 
+# Entrada do número de valores a serem calculados.
+
+num_valores = int(input('Informe quantos valores serão calculados: ').strip())
+
 # Entrada de numeros int ou float.
 
-numero_1 = float(input('Informe o primeiro número: ').strip().replace(',' , '.'))
-numero_2 = float(input('Informe o segundo número: ').strip().replace(',' , '.'))
+valores = []
+for i in range(num_valores):
+    valor = input(f'Informe o {i+1}º valor: ').strip().replace(',' , '.')
+    valores.append(valor)
 
 # Lista com as opções válidas.
 
@@ -17,30 +23,41 @@ escolha_operacao = ''
 # Escolha da operação Matematica com loop de verificação.
 
 while escolha_operacao not in  opcoes_validas:
-    escolha_operacao = input ('''Escolha uma das opações abaixo:\n
-( + ) = Soma
-( - ) = Subtração
-( / ) = divisão
-( x ) = Multiplicação\n
-Sua escolha? : ''').strip()
+    escolha_operacao = input ('''Escolha uma das opções abaixo:
+    ( + ) = Soma
+    ( - ) = Subtração
+    ( / ) = Divisão
+    ( x ) = Multiplicação\n
+    Sua escolha? : ''').strip()
     if escolha_operacao not in opcoes_validas:
         print('Escolha inválida! Tente novamente...')
-        sleep (1.0)
+        sleep(1.0)
 
-# Realizaçao do calculo com math case.
-
-match escolha_operacao:
-    case ('+'):
-        resultado = numero_1 + numero_2
-    case ('-'):
-        resultado = numero_1 - numero_2
-    case ('/'):
-        resultado = numero_1 / numero_2
-    case ('x'):
-        resultado = numero_1 * numero_2 
+# Realização do cálculo com math case.
+resultado = float(valores[0])
+for i in range(1, len(valores)):
+    valor = float(valores[i])
+    operador = escolha_operacao
+    match escolha_operacao:
+        case ('+'):
+            resultado = resultado + valor
+            valores_separados = ' + '.join(valores)
+        case ('-'):
+            resultado = resultado - valor
+            valores_separados = ' - '.join(valores)
+        case ('/'):
+            if valor == 0:
+                print("Erro! Divisão por zero não é permitido.")
+                resultado = None
+                break
+            else:
+                resultado = resultado / valor
+                valores_separados = ' / '.join(valores)
+        case ('x'):
+            resultado = resultado * valor 
+            valores_separados = ' x '.join(valores)
 
 # Mostra o resultado da conta.
 
-print (f'Resultado:\n{numero_1} {escolha_operacao} {numero_2} = {resultado}')
-
-        
+if resultado is not None:
+    print(f'Resultado:\n{valores_separados} = {resultado}')
