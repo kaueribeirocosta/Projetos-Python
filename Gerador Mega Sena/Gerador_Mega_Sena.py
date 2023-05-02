194d511fd5a961df1da89255c6da4de59d49c88a
Kauerico <ribeirokaue70@gmail.com>
2023-05-01 21:48:34 -0300

    Update Gerador_Mega_Sena.py

--- Gerador Mega Sena/Gerador_Mega_Sena.py
+++ Gerador Mega Sena/Gerador_Mega_Sena.py
@@ -11,5 +11,5 @@ qnt_jogos = int(input('Quantos Jogos serão?: ').strip())
 # 3 passo - sortear os jogos .
 print (f'-=-=-= Soteio dos Jogos -=-=-=')
 for sorteios in range(qnt_jogos): # Usamos um for com um range que o usuario pedir.
-    sorteio_jogos = choices(range(1, 61), k=6) # Chamanos a função choices para sortear.
+    sorteio_jogos = choices(range(1, 61), k=6) # Chamamos a função choices para sortear.
     print(f'{sorteios+1}° Jogo - {sorteio_jogos}') # Printamos os jogos.
