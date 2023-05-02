1c14ea5042b682306d2c27b9c63379f9805b16ad
Kauerico <ribeirokaue70@gmail.com>
2023-05-01 21:42:09 -0300

    Gerador da Mega Sena
    Gerador de jogos aleatórios da Mega Sena.

---
+++ Gerador_Mega_Sena.py
@@ -0,0 +1,15 @@
+print('==' * 10)
+print('Gerador da Mega Sena')
+print('==' * 10)
+
+# 1 passo - Importar a função choices da blib random.
+from random import choices
+
+# 2 passo -  Perguntar quantos jogos serão sorteados.
+qnt_jogos = int(input('Quantos Jogos serão?: ').strip())
+
+# 3 passo - sortear os jogos .
+print (f'-=-=-= Soteio dos Jogos -=-=-=')
+for sorteios in range(qnt_jogos): # Usamos um for com um range que o usuario pedir.
+    sorteio_jogos = choices(range(1, 61), k=6) # Chamanos a função choices para sortear.
+    print(f'{sorteios+1}° Jogo - {sorteio_jogos}') # Printamos os jogos.
