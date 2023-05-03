1f558c60e740be466bd86addddd252288392b07f
Kauerico <ribeirokaue70@gmail.com>
2023-05-03 14:42:12 -0300

    Palpites para Mega Sena

---
+++ Palpites Mega Sena/Palpites_Mega_Sena.py
@@ -0,0 +1,20 @@
+print('==' * 10)
+print('Gerador da Mega Sena')
+print('==' * 10)
+
+# 1 passo - Importar a função choices da blib random.
+from random import sample
+
+# 2 passo -  Perguntar quantos jogos serão sorteados.
+qnt_jogos = int(input('Quantos Jogos serão?: ').strip())
+
+# 3 passo - sortear os jogos .
+print (f'-=-=-= Soteio dos Jogos -=-=-=')
+for jogos in range(qnt_jogos): # Rodamos o for com quantidade de jogos.    
+    for sorteios in range(1): # Usamos um for com um range de 1.
+        sorteio_jogos = sample(range(1, 61), k=6) # Chamamos a função sample para sortear.
+    
+    print(f'{jogos+1}° Jogo - {sorteio_jogos}') # Printamos os jogos.
+
+print ('=-' * 5 , 'Boa Sorte', '=-' * 5)
+
